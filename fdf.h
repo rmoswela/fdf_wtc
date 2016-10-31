c0194badf155e372c2b09e8653022d02ce624dbd
Reuben MOSWELA <rmoswela@e5r6p11.jnb.42.fr>
2016-10-31 17:30:17 +0200

    added more variable in the structs to hold size and area of points

--- fdf.h
+++ fdf.h
@@ -6,9 +6,13 @@
 /*   By: rmoswela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/28 16:22:47 by rmoswela          #+#    #+#             */
-/*   Updated: 2016/10/29 16:56:58 by rmoswela         ###   ########.fr       */
+/*   Updated: 2016/10/31 16:21:04 by rmoswela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+# define EXIT 53
 
 #include <stdio.h>
 #include <unistd.h>
@@ -20,7 +24,7 @@ typedef struct		s_map
 {
 	double			x;
 	double			y;
-	int				z;
+	double			z;
 	struct s_map	*next;
 }					t_map;
 
@@ -36,5 +40,9 @@ typedef struct		s_env
 	void			*win;
 	int				x;
 	int				y;
+	int				area;
+	int				size;
 	t_map			*map;
 }					t_env;
+
+#endif
