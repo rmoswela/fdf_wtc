5b637f7f109e1967386e7314908ff6225950a949
Reuben MOSWELA <rmoswela@e5r6p11.jnb.42.fr>
2016-11-03 11:34:53 +0200

    corrected misspelt function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rmoswela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/28 13:17:50 by rmoswela          #+#    #+#             */
-/*   Updated: 2016/10/31 17:26:41 by rmoswela         ###   ########.fr       */
+/*   Updated: 2016/11/03 11:33:52 by rmoswela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int			main(int ac, char **av)
 {
 	int		fd;
 	char	*line;
-	t_env	*env;
+	t_env	env;
 	t_map	*point;
 
 	/*ensure that there is only one argument read*/
@@ -33,11 +33,11 @@ int			main(int ac, char **av)
 				/*initialize mlx*/
 				env.mlx = mlx_init();
 				/*make a new window*/
-				env.win = mlx_new_window(env.mlx, 1500, 1000, "FDF - " av[1]);
+				env.win = mlx_new_window(env.mlx, 1000, 1000, "FDF - av[1]");
 				/*function to draw 3D object*/
 				ft_draw_object(point, env);
 				/*mlx function to close the window*/
-				mlx_key_hook(env.win, ft_close, 0);
+				mlx_key_hook(env.win, ft_exit, 0);
 				/*mlx function to receive events and call the associated 
 				 * function to handle the received event*/
 				mlx_loop(env.mlx);
