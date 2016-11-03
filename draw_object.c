5aef2851151fdd9ea6c0ed56eafcc016e1284c96
Reuben MOSWELA <rmoswela@e5r6p11.jnb.42.fr>
2016-11-03 11:43:03 +0200

    corrected errors and changed the color

--- draw_object.c
+++ draw_object.c
@@ -6,24 +6,23 @@
 /*   By: rmoswela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/31 17:37:19 by rmoswela          #+#    #+#             */
-/*   Updated: 2016/11/02 15:01:52 by rmoswela         ###   ########.fr       */
+/*   Updated: 2016/11/03 11:41:46 by rmoswela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/*function*/
 t_map			*ft_point_to_place(t_map *new, t_env env)
 {
-	int			i;
+	int			loop;
 	t_map		*tmp;
 
-	i = 0;
+	loop = 0;
 	tmp = new;
-	while (i < env.x && tmp != NULL)
+	while (loop < env.x && tmp != NULL)
 	{
 		tmp = tmp->next;
-		i++;
+		loop++;
 	}
 	return (tmp);
 }
@@ -37,22 +36,22 @@ void			ft_draw_line(t_points p1, t_points p2, t_env env)
 	int			x_end;
 	int			y_end;
 
-	x = p1.x;
+	x_coord = p1.x;
 	x_end = p2.x;
 	y_end = p2.y;
 	if (p1.x == p2.x)
 		grad = 0;
 	else
-		grad = (p2.y - p1.y) / (p2.x - p1.x);
+		grad = (p2.y - p1.y) / (double) (p2.x - p1.x);
 	y_intercept = p1.y - grad * p1.x;
-	if (p2.x < p1.x (x_coord = p2.x))
-		x_end = p2.x;
-	while (x <= x_end && grad != 0 && (x = x + 0.1))
-		mlx_put_pixel(env.mlx, env.win, x + 350, \ 
-				grad * (x - 0.1) + y_intercept - 200, 0XFFFFFF);
+	if (p2.x < p1.x && (x_coord = p2.x))
+		x_end = p1.x;
+	while (x_coord <= x_end && grad != 0 && (x_coord = x_coord + 0.1))
+	{
+		mlx_pixel_put(env.mlx, env.win, x_coord + 350, grad * (x_coord - 0.1) + y_intercept - 200, 0XFF0000);
+	}
 	while (p1.y <= y_end && grad == 0 && (p1.y = p1.y + 0.1))
-		mlx_put_pixel(env.mlx, env.win, \ 
-				x_coord + 350, p1.y - 0.1 - 200, 0XFFFFFF)
+		mlx_pixel_put(env.mlx, env.win, x_coord + 350, p1.y - 0.1 - 200, 0XFF00FF);
 }
 
 /*function to draw columns*/
@@ -69,11 +68,11 @@ void			ft_draw_columns(t_map *new, t_env env)
 	{
 		p1.x = tmp->x + (800 - (8 * 20)) / 20;
 		p1.y = tmp->y + (800 / 2);
-		p2.x = tmp->x + (800 - (8 * 20)) / 20;
-		p2.y = tmp->y + (800 / 2);
+		p2.x = tmp1->x + (800 - (8 * 20)) / 20;
+		p2.y = tmp1->y + (800 / 2);
 		ft_draw_line(p1, p2, env);
 		tmp = tmp->next;
-		tmp1 = tmp->next;
+		tmp1 = tmp1->next;
 	}
 }
 
@@ -86,15 +85,16 @@ void			ft_draw_rows(t_map *new, t_env env)
 	t_points	p1;
 	t_points	p2;
 
-	loop = 0;
+	loop = 1;
 	tmp = new;
+	tmp1 = new;
 	tmp1 = tmp1->next;
 	while (tmp1)
 	{
 		p1.x = tmp->x + (800 - (8 * 20)) / 20;
-		p1.y = tmp->y + (800 /2);
-		p2.x = tmp->x + (800 - (8 * 20)) / 20;
-		p2.y = tmp->y + (800 /2);
+		p1.y = tmp->y + (800 / 2);
+		p2.x = tmp1->x + (800 - (8 * 20)) / 20;
+		p2.y = tmp1->y + (800 / 2);
 		if (loop < env.x)
 			ft_draw_line(p1, p2, env);
 		else
@@ -105,8 +105,9 @@ void			ft_draw_rows(t_map *new, t_env env)
 	}
 }
 
-void			ft_draw(t_map new, t_env env)
+/*function that calls functions that which draws the map*/
+void			ft_draw_object(t_map *new, t_env env)
 {
-	ft_draw_columsn(new, env);
+	ft_draw_columns(new, env);
 	ft_draw_rows(new, env);
 }
